f7b7316663d6e8f53de617431bb891b53fb5fe71
Faisal Karim <fk798@nyu.edu>
2021-04-21 23:30:02 -0400

    fixed small bug

--- P5/SDLProject/SDLProject/Entity.cpp
+++ P5/SDLProject/SDLProject/Entity.cpp
@@ -69,6 +69,9 @@ void Entity::CheckCollisionsX(Entity *objects, int objectCount)
 } }
 
 void Entity::CheckEnemyCollided(Entity *enemies, int enemyCount) {
+    collidedLeft = false;
+    collidedRight = false;
+    collidedTop = false;
     CheckCollisionsX(enemies, enemyCount);
     if (collidedLeft == true || collidedRight == true) isActive = false;
     CheckCollisionsY(enemies, enemyCount);
